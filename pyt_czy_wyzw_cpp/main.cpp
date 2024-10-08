d22f13a5b659113c24d17253ac0d661fb80c2545
mateusz-bogacz-collegiumwitelona <149972894+mateusz-bogacz-collegiumwitelona@users.noreply.github.com>
2024-10-08 14:19:24 +0200

    Update main.cpp

--- pyt_czy_wyzw_cpp/main.cpp
+++ pyt_czy_wyzw_cpp/main.cpp
@@ -1,19 +1,5 @@
-/**
- * @file main.cpp
- *
- * @brief Plik Ÿród³owy g³ównego programu.
- */
-
 #include "Quest.h"
 #include "Quest.cpp"
-
-/**
- * G³ówna funkcja programu.
- *
- * Tworzy obiekt klasy `Quest`, wywo³uje funkcje `Console()`, `Welcome()`, i `Menu()`, a nastêpnie zwraca 0.
- *
- * @return 0 po zakoñczeniu programu.
- */
 int main() {
     Quest q;
     q.Console();
